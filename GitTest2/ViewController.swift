d1ad92e0bad2a1f1f95c78a29bcc6682cc0df07c
Xavi Loinaz <Xavi@xavis-macbook-pro.local>
2016-05-21 16:18:00 -0700

    initial branch commit

--- GitTest2/ViewController.swift
+++ GitTest2/ViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Xavi Loinaz. All rights reserved.
 //
 
+// for branch1 I will only modify the ViewController.swift file
+
 import UIKit
 
 class ViewController: UIViewController {
